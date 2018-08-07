d4886c1f6fb2a025469e7825d744056d7511dda0
Francisco Hodge <Francisco.Hodge@barco.com>
2018-08-07 09:08:36 -0400

    npm update

---
+++ scripts/postinstall.js
@@ -0,0 +1,10 @@
+console.log('\x1b[36m%s\x1b[0m', `
+______________________________________\n`);
+
+console.log('\x1b[33m%s\x1b[0m', `Thank you for installing simple-keyboard !`);
+
+console.log('\x1b[36m%s\x1b[0m', `
+Please consider starring the repository so others can also find it.
+https://github.com/hodgef/simple-keyboard/stargazers
+______________________________________
+`);
