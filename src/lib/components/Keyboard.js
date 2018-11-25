bc420bb60caf9b971772d421d4a9f9c6f7bd1833
Francisco Hodge <hello@franciscohodge.com>
2018-11-24 20:11:48 -0500

    Adding ternary condition for preventMouseDownDefault

--- src/lib/components/Keyboard.js
+++ src/lib/components/Keyboard.js
@@ -63,7 +63,7 @@ class SimpleKeyboard {
     this.options.layoutName = this.options.layoutName || "default";
     this.options.theme = this.options.theme || "hg-theme-default";
     this.options.inputName = this.options.inputName || "default";
-    this.options.preventMouseDownDefault = this.options.preventMouseDownDefault || true;
+    this.options.preventMouseDownDefault = this.options.preventMouseDownDefault === false ? false : true;
 
     /**
      * @type {object} Classes identifying loaded plugins
