dc085b377d289be8026d1a4722424b1150fe2a34
Francisco Hodge <hello@franciscohodge.com>
2019-01-27 20:02:29 -0500

    Updated dev dependencies

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   simple-keyboard v2.14.0
+ *   simple-keyboard v2.14.1
  *   https://github.com/hodgef/simple-keyboard
  * 
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
