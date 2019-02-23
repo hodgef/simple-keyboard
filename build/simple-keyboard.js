92d1e822a5930bdabbfaf9dd867bcae79d7c6f1e
Francisco Hodge <hello@franciscohodge.com>
2019-02-23 14:44:13 -0500

    Updated dev dependencies

--- build/simple-keyboard.js
+++ build/simple-keyboard.js
@@ -1,11 +1,11 @@
 /*!
  * 
- *   simple-keyboard v2.15.2 (Non-minified build)
+ *   simple-keyboard v2.15.3 (Non-minified build)
  *   https://github.com/hodgef/simple-keyboard
  * 
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
  * 
- *   This source code is licensed under a dual license system (MIT or GPL-3.0), as found in the
+ *   This source code is licensed under the MIT license found in the
  *   LICENSE file in the root directory of this source tree.
  *   
  */
