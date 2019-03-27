737d1fe2f25eb54c0c064d2f76c7c19aa0abbc81
Francisco Hodge <hello@franciscohodge.com>
2019-03-26 23:47:58 -0400

    Build update

--- build/simple-keyboard.js
+++ build/simple-keyboard.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   simple-keyboard v2.20.2 (Non-minified build)
+ *   simple-keyboard v2.20.3 (Non-minified build)
  *   https://github.com/hodgef/simple-keyboard
  * 
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
