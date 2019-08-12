e6f8f47975f539454b7482f5105d2b123c61557e
Francisco Hodge <hello@franciscohodge.com>
2019-08-11 21:09:51 -0400

    npm update

--- build/css/simple-keyboard.css
+++ build/css/simple-keyboard.css
@@ -1,6 +1,6 @@
 /*!
  * 
- *   simple-keyboard v2.25.0 (Non-minified build)
+ *   simple-keyboard v2.25.1 (Non-minified build)
  *   https://github.com/hodgef/simple-keyboard
  * 
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   simple-keyboard v2.25.0
+ *   simple-keyboard v2.25.1
  *   https://github.com/hodgef/simple-keyboard
  * 
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
