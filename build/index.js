d186fb1e09505829b805d10c69d744d481b92324
Francisco Hodge <hello@franciscohodge.com>
2019-08-17 16:27:06 -0400

    Optimization: Removing non-minified dist

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   simple-keyboard v2.25.4
+ *   simple-keyboard v2.26.0
  *   https://github.com/hodgef/simple-keyboard
  * 
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
