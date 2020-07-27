1d8c67ff1ab1af1991e3d48ff2e7410a1c8ca873
Francisco Hodge <hello@franciscohodge.com>
2020-07-27 15:35:41 +0000

    Build update

--- build/css/index.css
+++ build/css/index.css
@@ -1,6 +1,6 @@
 /*!
  * 
- *   simple-keyboard v2.29.74
+ *   simple-keyboard v2.29.75
  *   https://github.com/hodgef/simple-keyboard
  * 
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef)
