684afce4af140c46ac7492eeab2ec58fc1f47769
Francisco Hodge <hello@franciscohodge.com>
2021-03-21 12:49:01 -0400

    Adjust CandidateBox style

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   simple-keyboard v3.0.0-beta.1
+ *   simple-keyboard v3.0.0-beta.2
  *   https://github.com/hodgef/simple-keyboard
  *
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef) and project contributors.
