cba1441b29306ac8f6425578ede55414b0a28b3d
Francisco Hodge <hello@franciscohodge.com>
2021-05-21 09:11:05 +0000

    Build update

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   simple-keyboard v3.1.9
+ *   simple-keyboard v3.1.10
  *   https://github.com/hodgef/simple-keyboard
  *
  *   Copyright (c) Francisco Hodge (https://github.com/hodgef) and project contributors.
