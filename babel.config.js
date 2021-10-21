178e21e247ee43ba3cd0ca3eb8c11067b8912a46
Francisco Hodge <hello@franciscohodge.com>
2021-10-21 08:06:23 +0000

    Build update

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,25 @@
 module.exports = {
   presets: [
-    ["@babel/env", {
-      corejs:"3",
-      useBuiltIns: 'usage',
-      "targets": {
-        "browsers": [
-          "edge >= 16",
-          "safari >= 9",
-          "firefox >= 57",
-          "ie >= 11",
-          "ios >= 9",
-          "chrome >= 49"
-        ]
+    [
+      "@babel/env",
+      {
+        corejs: "3",
+        useBuiltIns: "usage",
+        targets: {
+          browsers: [
+            "edge >= 16",
+            "safari >= 9",
+            "firefox >= 57",
+            "ie >= 11",
+            "ios >= 9",
+            "chrome >= 49",
+          ],
+        },
       },
-    }]
+    ],
   ],
   plugins: [
     ["@babel/plugin-proposal-class-properties"],
-    ["@babel/plugin-transform-typescript"]
-  ]
+    ["@babel/plugin-transform-typescript"],
+  ],
 };
