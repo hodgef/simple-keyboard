9a3aae64852116efba47c40b8070ed5b8902c832
Francisco Hodge <hello@franciscohodge.com>
2024-09-02 08:53:36 +0000

    Build update

--- build/index.modern.js
+++ build/index.modern.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   simple-keyboard v3.7.105 (index.modern.js - Modern Browsers bundle)
+ *   simple-keyboard v3.7.106 (index.modern.js - Modern Browsers bundle)
  *   https://github.com/hodgef/simple-keyboard
  *
  *   NOTE: This modern browsers bundle (index.modern.js) removes all polyfills
