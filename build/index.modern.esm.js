8dc174862b0708afff7807e421386f2055ada780
Francisco Hodge <hello@franciscohodge.com>
2026-05-11 10:22:07 +0000

    Build update

--- build/index.modern.esm.js
+++ build/index.modern.esm.js
@@ -1,6 +1,6 @@
 /*!
  * 
- *   simple-keyboard v3.8.140 (index.modern.esm.js - Modern Browsers bundle, ESM output)
+ *   simple-keyboard v3.8.141 (index.modern.esm.js - Modern Browsers bundle, ESM output)
  *   https://github.com/hodgef/simple-keyboard
  *
  *   NOTE: This modern browsers bundle (index.modern.esm.js) removes all polyfills
